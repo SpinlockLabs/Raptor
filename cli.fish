b64669700bfb345d06e24a5f846f091f573859cd
Logan Gorence <loganjohngorence@gmail.com>
2017-01-20 23:20:19 -0800

    Add QEMU functions to fish.

--- cli.fish
+++ cli.fish
@@ -3,14 +3,22 @@ function build_arm
     mkdir -p out/arm
     arm-none-eabi-gcc -mcpu=arm1176jzf-s -fpic -ffreestanding -c kernel/arm/rpi/boot.s -o out/arm/boot.o
     arm-none-eabi-gcc -mcpu=arm1176jzf-s -fpic -ffreestanding -std=gnu99 -c kernel/arm/rpi/entry.c -o out/arm/entry.o -O2 -Wall -Wextra
-    arm-none-eabi-gcc -T kernel/arm/rpi/linker.ld -o out/arm/raptor.elf -ffreestanding -O2 -nostdlib out/arm/boot.o out/arm/entry.o
+    arm-none-eabi-gcc -T kernel/arm/rpi/linker.ld -o out/arm/raptor.bin -ffreestanding -O2 -nostdlib out/arm/boot.o out/arm/entry.o
 end
 
 function build_x86
     mkdir -p out/x86
     i686-elf-as kernel/x86/boot.s -o out/x86/boot.o
     i686-elf-gcc -c kernel/x86/entry.c -o out/x86/entry.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-    i686-elf-gcc -T kernel/x86/linker.ld -o out/x86/raptor.elf -ffreestanding -O2 -nostdlib out/x86/boot.o out/x86/entry.o
+    i686-elf-gcc -T kernel/x86/linker.ld -o out/x86/raptor.bin -ffreestanding -O2 -nostdlib out/x86/boot.o out/x86/entry.o
+end
+
+function qemu_arm
+    qemu-system-arm -kernel out/arm/raptor.bin -m 256 -M raspi2 -serial stdio
+end
+
+function qemu_x86
+    qemu-system-i386 -kernel out/x86/raptor.bin
 end
 
 function clean
--- qemu.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-qemu-system-arm -kernel raptor.elf -m 256 -M raspi2 -serial stdio
-
