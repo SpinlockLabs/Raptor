b52edc394dfadc488922c18d7bf7aacbffe1bf8a
Logan Gorence <loganjohngorence@gmail.com>
2017-01-23 23:54:37 -0800

    Making progress on keyboard driver.

--- kernel/x86/keyboard.c
+++ kernel/x86/keyboard.c
@@ -21,18 +21,37 @@ static int keyboard_callback(regs_t *regs) {
         }
     } while(1);
 
+    if ((c & 128) == 128) {
+        vga_writestring("released\n");
+    }
+
     char kt = key_types[c];
     switch (kt) {
         case N:
-            vga_writestring("N\n");
+            vga_putchar((shift ? kb_usu[c] : kb_usl[c]));
             break;
         case S:
-            vga_writestring("S\n");
+            switch (c) {
+                case 0x2A:
+                case 0x36:
+                    shift = true;
+                    break;
+                case 0xAA:
+                case 0xB6:
+                    shift = false;
+                    break;
+            }
             break;
         case F:
             vga_writestring("F\n");
             break;
     }
+
+    char* cs;
+    itoa(c, cs, 16);
+    vga_writestring("Code ");
+    vga_writestring(cs);
+    vga_writestring("\n");
 
     return 1;
 }
