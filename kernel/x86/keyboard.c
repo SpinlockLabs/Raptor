279559d8bc54c93cc6009aaac5e39b06e818e526
Logan Gorence <loganjohngorence@gmail.com>
2017-01-24 23:50:01 -0800

    Clean up a bit of code, I think there is something weird going on with QEMU.

--- kernel/x86/keyboard.c
+++ kernel/x86/keyboard.c
@@ -12,39 +12,26 @@
 static int keyboard_callback(regs_t *regs) {
     char c = 0;
 
-    do {
-        if (inb(0x60) != c) {
-            c = inb(0x60);
-            if (c > 0) {
-                break;
-            }
-        }
-    } while(1);
+    c = inb(0x60);
 
-    if ((c & 128) == 128) {
-        vga_writestring("released\n");
+    switch (c) {
+        case 0x2A:
+        case 0x36:
+            shift = true;
+            break;
+        case 0xAA:
+        case 0xB6:
+            shift = false;
+            break;
     }
-
-    char kt = key_types[c];
-    switch (kt) {
-        case N:
-            vga_putchar((shift ? kb_usu[c] : kb_usl[c]));
-            break;
-        case S:
-            switch (c) {
-                case 0x2A:
-                case 0x36:
-                    shift = true;
-                    break;
-                case 0xAA:
-                case 0xB6:
-                    shift = false;
-                    break;
-            }
-            break;
-        case F:
-            vga_writestring("F\n");
-            break;
+    
+    {
+        char kt = key_types[c];
+        switch (kt) {
+            case N:
+                vga_putchar((shift ? kb_usu[c] : kb_usl[c]));
+                break;
+        }
     }
 
     char* cs;
