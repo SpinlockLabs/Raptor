edb83ea21eadd4dc280306f8a47a42a853285df4
Kenneth Endfinger <kaendfinger@gmail.com>
2017-06-25 15:15:48 -0400

    CMake toolchain definition now defines CMAKE_SYSTEM_PROCESSOR

--- build/cmake/toolchains/arm.cmake
+++ build/cmake/toolchains/arm.cmake
@@ -1,6 +1,7 @@
 include(CMakeForceCompiler)
 
 set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
 set(CMAKE_LINKER arm-none-eabi-ld)
