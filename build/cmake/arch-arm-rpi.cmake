ef31c248d457690c5af2d8f46d7f1e227c35ee67
Kenneth Endfinger <kaendfinger@gmail.com>
2017-06-25 17:38:07 -0400

    Rework CMake to make cflags propogate.

--- build/cmake/arch-arm-rpi.cmake
+++ build/cmake/arch-arm-rpi.cmake
@@ -1,29 +1,26 @@
-raptor_set_arch("arm-rpi" "arch/arm/rpi")
+arch("arm-rpi" "arch/arm/rpi")
 
-raptor_set_arch_cflags(
-        "-Wall \
-        -std=gnu11 \
-        -mcpu=arm1176jzf-s \
-        -ffreestanding \
-        -O2 \
-        -fpic \
-        -Wno-unused-command-line-argument"
+cflags(
+  -mcpu=arm1176jzf-s
+  -ffreestanding
+  -O2
+  -fpic
+  -Wno-unused-command-line-argument
 )
 
-raptor_set_arch_ldflags(
-        "-nostartfiles \
-        -mcpu=arm1176jzf-s \
-        -Wno-unused-command-line-argument \
-        -ffreestanding \
-        -O2 \
-        -T${KERNEL_DIR}/arch/arm/rpi/linker.ld"
+ldflags(
+  "-T${KERNEL_DIR}/arch/arm/rpi/linker.ld"
+)
+
+set(QEMU_FLAGS
+  qemu-system-arm
+    -kernel "${CMAKE_BINARY_DIR}/raptor.bin"
+    -m 256
+    -M raspi2
+    -serial stdio
 )
 
 add_custom_target(qemu
-        COMMAND "qemu-system-arm"
-          "-kernel" "${CMAKE_BINARY_DIR}/raptor.bin"
-          "-m" "256"
-          "-M" "raspi2"
-          "-serial" "stdio"
-        DEPENDS raptor.bin
+  COMMAND ${QEMU_FLAGS}
+  DEPENDS raptor.bin
 )
--- build/cmake/arch.cmake
+++ build/cmake/arch.cmake
@@ -1,4 +1,4 @@
-function(raptor_set_arch ARCH SRC_DIR)
+function(arch ARCH SRC_DIR)
   file(GLOB_RECURSE ARCH_SRC
     "${KERNEL_DIR}/${SRC_DIR}/*.c"
     "${KERNEL_DIR}/${SRC_DIR}/*.h"
@@ -8,17 +8,9 @@ function(raptor_set_arch ARCH SRC_DIR)
   )
 
   add_executable(raptor.bin ${ARCH_SRC} ${KERNEL_COMMON_SRC})
-  add_dependencies(raptor.bin lox-kernel)
   target_link_libraries(raptor.bin lox-kernel)
-  target_include_directories(raptor.bin BEFORE PRIVATE ${RAPTOR_DIR})
 endfunction()
 
-function(raptor_set_arch_cflags CFLAGS_TO_ADD)
-  set_target_properties(raptor.bin PROPERTIES COMPILE_FLAGS ${CFLAGS_TO_ADD})
-  set_target_properties(lox-kernel PROPERTIES COMPILE_FLAGS ${CFLAGS_TO_ADD})
-endfunction()
-
-function(raptor_set_arch_ldflags LDFLAGS_TO_ADD)
-  set_target_properties(raptor.bin PROPERTIES LINK_FLAGS ${LDFLAGS_TO_ADD})
-  set_target_properties(lox-kernel PROPERTIES LINK_FLAGS ${LDFLAGS_TO_ADD})
+function(ldscript LDSCRIPT)
+  set_target_properties(raptor.bin PROPERTIES LINK_FLAGS "-T${LDSCRIPT}")
 endfunction()
