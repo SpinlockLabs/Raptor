a235e403d27f4ddd282a9f39f765192b10954c10
Logan Gorence <loganjohngorence@gmail.com>
2017-07-14 11:11:29 -0700

    cmake: If sfdisk isn't in path, assume /usr/sbin/sfdisk.

--- build/scripts/gendiskimg.sh
+++ build/scripts/gendiskimg.sh
@@ -2,17 +2,23 @@
 set -e
 
 MKFAT="mkfs.fat"
+SFDISK="sfdisk"
 
 if ! which "${MKFAT}" > /dev/null 2>&1
 then
   MKFAT="/usr/sbin/mkfs.fat"
 fi
 
+if ! which "${SFDISK}" > /dev/null 2>&1
+then
+  SFDISK="/usr/sbin/sfdisk"
+fi
+
 if [[ ! -f raptor.img ]]; then
   dd if=/dev/zero of=raptor.img iflag=fullblock bs=1M count=100
   "${MKFAT}" raptor.img
-  echo -e 'label: dos\nstart=2048, type=83' | sfdisk -a raptor.img
-  sfdisk -A raptor.img 1
+  echo -e 'label: dos\nstart=2048, type=83' | "${SFDISK}" -a raptor.img
+  "${SFDISK}" -A raptor.img 1
   sudo losetup -P /dev/loop0 raptor.img
   sudo "${MKFAT}" /dev/loop0p1
   sudo losetup -d /dev/loop0
