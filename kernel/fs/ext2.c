f31683f3213ef95ff34de9361dfe44b3c8b81552
Kenneth Endfinger <kaendfinger@gmail.com>
2017-07-15 21:52:36 -0400

    ext2: WIP

--- kernel/fs/ext2.c
+++ kernel/fs/ext2.c
@@ -1,1 +1,126 @@
+#include <kernel/disk/block.h>
+#include <kernel/spin.h>
+#include <liblox/string.h>
 #include "ext2.h"
+#include "vfs.h"
+
+typedef struct ext2_fs {
+    ext2_superblock_t* superblock;
+    ext2_bgdescriptor_t* block_groups;
+    block_device_t* device;
+    size_t block_size;
+
+    uint32_t pointers_per_block;
+    uint32_t inodes_per_group;
+    uint32_t block_group_count;
+
+    ext2_disk_cache_entry_t* disk_cache;
+    uint32_t cache_entries;
+    uint32_t cache_time;
+
+    spin_lock_t lock;
+
+    uint8_t bgd_block_span;
+    uint8_t bdg_offset;
+    uint32_t inode_size;
+
+    uint8_t* cache_data;
+} ext2_fs_t;
+
+static uint get_cache_time(ext2_fs_t* this) {
+    return this->cache_time++;
+}
+
+static fs_error_t cache_flush_dirty(ext2_fs_t* this, uint ent_no) {
+    block_device_error_t error = block_device_write(
+      this->device,
+      this->disk_cache[ent_no].block_no * this->block_size,
+      this->disk_cache[ent_no].block,
+      this->block_size
+    );
+
+    if (error != BLOCK_DEVICE_ERROR_OK) {
+        return FS_ERROR_IO_FAILED;
+    }
+    this->disk_cache[ent_no].dirty = 0;
+    return FS_ERROR_OK;
+}
+
+static fs_error_t read_block(ext2_fs_t* this, uint block_id, uint8_t* buffer) {
+    if (block_id == 0) {
+        return FS_ERROR_BAD_CALL;
+    }
+
+    spin_lock(this->lock);
+    if (this->disk_cache == NULL) {
+        block_device_error_t error = block_device_read(
+            this->device,
+            block_id * this->block_size,
+            buffer,
+            this->block_size
+        );
+
+        spin_unlock(this->lock);
+
+        if (error != BLOCK_DEVICE_ERROR_OK) {
+            return FS_ERROR_IO_FAILED;
+        }
+
+        return FS_ERROR_OK;
+    }
+
+    int oldest = -1;
+    uint oldest_age = UINT32_MAX;
+
+    for (uint i = 0; i < this->cache_entries; i++) {
+        if (this->disk_cache[i].block_no == block_id) {
+            this->disk_cache[i].last_use = get_cache_time(this);
+            memcpy(buffer, this->disk_cache[i].block, this->block_size);
+            spin_unlock(this->lock);
+            return FS_ERROR_OK;
+        }
+
+        if (this->disk_cache[i].last_use < oldest_age) {
+            oldest = i;
+            oldest_age = this->disk_cache[i].last_use;
+        }
+    }
+
+    if (this->disk_cache[oldest].dirty) {
+        cache_flush_dirty(this, (uint) oldest);
+    }
+
+    block_device_error_t error = block_device_read(
+        this->device,
+        block_id * this->block_size,
+        this->disk_cache[oldest].block,
+        this->block_size
+    );
+
+    if (error != BLOCK_DEVICE_ERROR_OK) {
+        spin_unlock(this->lock);
+        return FS_ERROR_IO_FAILED;
+    }
+
+    memcpy(buffer, this->disk_cache[oldest].block, this->block_size);
+
+    this->disk_cache[oldest].block_no = block_id;
+    this->disk_cache[oldest].last_use = get_cache_time(this);
+    this->disk_cache[oldest].dirty = 0;
+
+    spin_unlock(this->lock);
+
+    return FS_ERROR_OK;
+}
+
+used static fs_node_t* ext2_mount(block_device_t* block) {
+    ext2_fs_t* this = zalloc(sizeof(ext2_fs_t));
+
+    this->device = block;
+    this->block_size = 1024;
+
+    this->superblock = zalloc(this->block_size);
+
+    read_block(this, 1, (uint8_t*) this->superblock);
+    return NULL;
+}
--- kernel/fs/ext2.h
+++ kernel/fs/ext2.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <liblox/common.h>
+
 #include <stdint.h>
+
+#define EXT2_SUPER_MAGIC 0xEF53
+#define EXT2_DIRECT_BLOCKS 12
 
 /* Super block struct. */
 struct ext2_superblock {
@@ -9,7 +14,7 @@ struct ext2_superblock {
     uint32_t r_blocks_count;
     uint32_t free_blocks_count;
     uint32_t free_inodes_count;
-    uint32_t first_dblock;
+    uint32_t first_data_block;
     uint32_t log_block_size;
     uint32_t log_frag_size;
     uint32_t blocks_per_group;
@@ -69,6 +74,91 @@ struct ext2_superblock {
     uint32_t default_mount_options;
     uint32_t first_meta_bg;
     uint8_t _unused[760];
-} __attribute__ ((packed));
+
+} packed;
 
 typedef struct ext2_superblock ext2_superblock_t;
+
+/* Block group descriptor. */
+struct ext2_bgdescriptor {
+    uint32_t block_bitmap;
+    uint32_t inode_bitmap;        // block no. of inode bitmap
+    uint32_t inode_table;
+    uint16_t free_blocks_count;
+    uint16_t free_inodes_count;
+    uint16_t used_dirs_count;
+    uint16_t pad;
+    uint8_t reserved[12];
+} packed;
+
+typedef struct ext2_bgdescriptor ext2_bgdescriptor_t;
+
+/* File Types */
+#define EXT2_S_IFSOCK    0xC000
+#define EXT2_S_IFLNK    0xA000
+#define EXT2_S_IFREG    0x8000
+#define EXT2_S_IFBLK    0x6000
+#define EXT2_S_IFDIR    0x4000
+#define EXT2_S_IFCHR    0x2000
+#define EXT2_S_IFIFO    0x1000
+
+/* setuid, etc. */
+#define EXT2_S_ISUID    0x0800
+#define EXT2_S_ISGID    0x0400
+#define EXT2_S_ISVTX    0x0200
+
+/* rights */
+#define EXT2_S_IRUSR    0x0100
+#define EXT2_S_IWUSR    0x0080
+#define EXT2_S_IXUSR    0x0040
+#define EXT2_S_IRGRP    0x0020
+#define EXT2_S_IWGRP    0x0010
+#define EXT2_S_IXGRP    0x0008
+#define EXT2_S_IROTH    0x0004
+#define EXT2_S_IWOTH    0x0002
+#define EXT2_S_IXOTH    0x0001
+
+/* This is not actually the inode table.
+ * It represents an inode in an inode table on disk. */
+struct ext2_inodetable {
+    uint16_t mode;
+    uint16_t uid;
+    uint32_t size;            // file length in byte.
+    uint32_t atime;
+    uint32_t ctime;
+    uint32_t mtime;
+    uint32_t dtime;
+    uint16_t gid;
+    uint16_t links_count;
+    uint32_t blocks;
+    uint32_t flags;
+    uint32_t osd1;
+    uint32_t block[15];
+    uint32_t generation;
+    uint32_t file_acl;
+    uint32_t dir_acl;
+    uint32_t faddr;
+    uint8_t osd2[12];
+} packed;
+
+typedef struct ext2_inodetable ext2_inodetable_t;
+
+/* Represents directory entry on disk. */
+struct ext2_dir {
+    uint32_t inode;
+    uint16_t rec_len;
+    uint8_t name_len;
+    uint8_t file_type;
+    char name[];        /* Actually a set of characters, at most 255 bytes */
+} packed;
+
+typedef struct ext2_dir ext2_dir_t;
+
+typedef struct {
+    uint32_t block_no;
+    uint32_t last_use;
+    uint8_t dirty;
+    uint8_t* block;
+} ext2_disk_cache_entry_t;
+
+typedef int (*ext2_block_io_t)(void*, uint32_t, uint8_t*);
