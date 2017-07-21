c5f903162aae553a85ef38dce86e538477eeb4a1
Kenneth Endfinger <kaendfinger@gmail.com>
2017-07-21 10:41:47 -0400

    kernel: process changes

--- kernel/process/process.c
+++ kernel/process/process.c
@@ -6,15 +6,31 @@
 
 static tree_t* process_tree = NULL;
 static list_t* process_list = NULL;
+static list_t* wait_queue = NULL;
 
+static process_t* kidle_process = NULL;
 static volatile pid_t next_pid = 0;
 static volatile process_t* current_process = NULL;
 
 static spin_lock_t tree_lock = {0};
 
+static process_t* create_kidle(void) {
+    spin_lock(tree_lock);
+    process_t* proc = zalloc(sizeof(process_t));
+    proc->name = "[kernel]";
+    proc->pid = 0;
+    proc->cmdline = NULL;
+    proc->status = PROCESS_RUNNING;
+    return proc;
+}
+
 void process_tree_init(void) {
+    spin_init(tree_lock);
     process_tree = tree_create();
     process_list = list_create();
+    wait_queue = list_create();
+
+    kidle_process = create_kidle();
 }
 
 process_t* process_get_current(void) {
@@ -23,4 +39,12 @@ process_t* process_get_current(void) {
 
 pid_t process_get_next_pid(void) {
     return (pid_t) next_pid++;
+}
+
+list_t* process_get_all(void) {
+    return process_list;
+}
+
+tree_t* process_get_tree(void) {
+    return process_tree;
 }
--- kernel/process/process.h
+++ kernel/process/process.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #include <liblox/common.h>
+#include <liblox/list.h>
+#include <liblox/tree.h>
 
 #include <stdint.h>
 
 #include <kernel/arch.h>
-#include <kernel/scheduler/scheduler.h>
 
 /* A process identifier. */
 typedef uint pid_t;
@@ -16,7 +17,10 @@ typedef enum process_status {
     PROCESS_RUNNING,
 
     /* The process is waiting. */
-    PROCESS_WAITING
+    PROCESS_WAITING,
+
+    /* The process is ready to run. */
+    PROCESS_READY
 } process_status_t;
 
 /* Represents a process that can be scheduled. */
@@ -33,9 +37,14 @@ typedef struct process {
     /* Architecture-specific extensions. */
     arch_process_extensions_t arch;
 
-    /* Scheduler state. */
-    scheduler_state_t scheduler;
+    /* Current process status. */
+    process_status_t status;
+
+    /* CPU register state. */
+    cpu_registers_t registers;
 } process_t;
 
 pid_t process_get_next_pid(void);
 process_t* process_get_current(void);
+list_t* process_get_all(void);
+tree_t* process_get_tree(void);
