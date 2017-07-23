752606163852b745d32fa4e435015d4c5d4e34c4
Kenneth Endfinger <kaendfinger@gmail.com>
2017-07-23 14:49:53 -0400

    rtl8139: WIP implementation.

---
+++ kernel/arch/x86/devices/rtl/dev.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define RTL_PORT_MAC     0x00
+#define RTL_PORT_MAR     0x08
+#define RTL_PORT_TXSTAT  0x10
+#define RTL_PORT_TXBUF   0x20
+#define RTL_PORT_RBSTART 0x30
+#define RTL_PORT_CMD     0x37
+#define RTL_PORT_RXPTR   0x38
+#define RTL_PORT_RXADDR  0x3A
+#define RTL_PORT_IMR     0x3C
+#define RTL_PORT_ISR     0x3E
+#define RTL_PORT_TCR     0x40
+#define RTL_PORT_RCR     0x44
+#define RTL_PORT_RXMISS  0x4C
+#define RTL_PORT_CONFIG 0x52
--- kernel/arch/x86/devices/rtl/rtl8139.c
+++ kernel/arch/x86/devices/rtl/rtl8139.c
@@ -1,39 +1,154 @@
 #include "rtl8139.h"
+#include "dev.h"
+
+#include <liblox/io.h>
+#include <liblox/list.h>
 
 #include <kernel/arch/x86/io.h>
 #include <kernel/arch/x86/devices/pci/pci.h>
-#include <kernel/arch/x86/heap.h>
-#include <liblox/io.h>
+#include <kernel/spin.h>
+#include <kernel/network/iface.h>
+#include <liblox/string.h>
+#include <kernel/arch/x86/arch-x86.h>
 
-static void rtl8139_init(uint32_t device) {
-    uint32_t rtl_bar0 = pci_read_field(device, PCI_BAR0, 4);
-    uint32_t rtl_bar1 = pci_read_field(device, PCI_BAR1, 4);
-    uint32_t rtl_iobase = 0x00000000;
-
-    if (rtl_bar0 & 0x00000001) {
-        rtl_iobase = rtl_bar0 & 0xFFFFFFFC;
-    }
-
-    uint16_t command_reg = (uint16_t) pci_read_field(device, PCI_COMMAND, 2);
-    command_reg |= (1 << 2);
-    pci_write_field(device, PCI_COMMAND, 2, command_reg);
-
+typedef struct rtl8139_iface {
+    network_iface_t* iface;
+    list_t* net_queue;
     uint8_t mac[6];
-    for (int i = 0; i < 6; ++i) {
-        mac[i] = ins(rtl_iobase + i);
+    int irq;
+    uint32_t io_base;
+    uint8_t* rx_buffer;
+    uint8_t* tx_buffer[5];
+    uintptr_t rx_phys;
+    uintptr_t tx_phys[5];
+    uint32_t current_rx;
+    int dirty_tx;
+    int next_tx;
+    spin_lock_t net_queue_lock;
+    spin_lock_t tx_buf_lock;
+    uint8_t* last_packet;
+} rtl8139_iface_t;
+
+typedef struct rtl_net_buf {
+    uint8_t* buffer;
+    size_t size;
+} rtl_net_buf_t;
+
+static int next_tx_buf(rtl8139_iface_t* rtl) {
+    int out;
+    spin_lock(rtl->tx_buf_lock);
+    out = rtl->next_tx;
+    rtl->next_tx++;
+    if (rtl->next_tx == 4) {
+        rtl->next_tx = 0;
+    }
+    spin_unlock(rtl->tx_buf_lock);
+    return out;
+}
+
+static void rtl_dequeue_task(void* extra) {
+    rtl8139_iface_t* rtl = extra;
+
+    spin_lock(rtl->net_queue_lock);
+    if (rtl->net_queue->size == 0) {
+        return;
     }
 
-    printf(DEBUG "%2x:%2x:%2x:%2x:%2x:%2x\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    list_node_t* node = list_dequeue(rtl->net_queue);
+    rtl_net_buf_t* value = node->value;
+    free(node);
 
-    // Turn on
-    outb((uint16_t) (rtl_iobase + 0x52), 0x0);
+    network_iface_t* iface = rtl->iface;
 
-    // Software reset
-    outb(rtl_iobase + 0x37, 0x10);
-    while ((inb(rtl_iobase + 0x37) & 0x10) != 0) {}
+    if (iface->handle_receive != NULL) {
+        iface->handle_receive(iface, value->buffer, value->size);
+    }
 
-    //uintptr_t rx_buffer = kpmalloc_a(8192 + 16);
-    //outs((uint16_t) (rtl_iobase + 0x30), (uint16_t) rx_buffer);
+    free(value);
+    spin_unlock(rtl->net_queue_lock);
+}
+
+static void rtl_enqueue(rtl8139_iface_t* rtl, rtl_net_buf_t* buf) {
+    spin_lock(rtl->net_queue_lock);
+    list_add(rtl->net_queue, buf);
+    spin_unlock(rtl->net_queue_lock);
+}
+
+static void rtl_do_send(rtl8139_iface_t* rtl, uint8_t* buffer, size_t size) {
+    int tx = next_tx_buf(rtl);
+    memcpy(rtl->tx_buffer[tx], buffer, size);
+    outl((uint16_t) (rtl->io_base + RTL_PORT_TXBUF + 4 * tx), rtl->tx_phys[tx]);
+    outl((uint16_t) (rtl->io_base + RTL_PORT_TXSTAT + 4 * tx), size);
+}
+
+static list_t* rtl_list = NULL;
+
+static void rtl_device_interrupt(rtl8139_iface_t* rtl) {
+    uint16_t status = ins((uint16_t) (rtl->io_base + RTL_PORT_ISR));
+
+    if (!status) {
+        return;
+    }
+
+    outs((uint16_t) (rtl->io_base + RTL_PORT_ISR), status);
+
+    if (status & 0x01 || status & 0x02) {
+        while ((inb((uint16_t) (rtl->io_base + RTL_PORT_CMD)) & 0x01) == 0) {
+            int offset = rtl->current_rx % 0x2000;
+
+            uint32_t* buf_start = (uint32_t*)((uintptr_t) rtl->rx_buffer + offset);
+            uint32_t rx_status = buf_start[0];
+            int rx_size = rx_status >> 16;
+
+            if (!(rx_status & (0x0020 | 0x0010 | 0x0004 | 0x0002))) {
+                uint8_t* bufd = (uint8_t*) &(buf_start[1]);
+
+                rtl->last_packet = malloc((size_t) rx_size);
+
+                uintptr_t packet_end = (uintptr_t) bufd + rx_size;
+
+                if (packet_end > (uintptr_t) rtl->rx_buffer + 0x2000) {
+                    size_t s = ((uintptr_t) rtl->rx_buffer + 0x2000) - (uintptr_t) bufd;
+                    memcpy(rtl->last_packet, bufd, (size_t) rx_size);
+                    memcpy(
+                        (void*) ((uintptr_t) rtl->last_packet + s),
+                        rtl->rx_buffer,
+                        rx_size - s
+                    );
+                } else {
+                    memcpy(rtl->last_packet, bufd, (size_t) rx_size);
+                }
+
+                rtl_net_buf_t* buf = zalloc(sizeof(rtl_net_buf_t));
+                buf->buffer = rtl->last_packet;
+                buf->size = (size_t) rx_size;
+                rtl_enqueue(rtl, buf);
+            }
+        }
+    }
+
+    if (status & 0x08 || status & 0x04) {
+        uint i = inl((uint16_t) (rtl->io_base + RTL_PORT_TXSTAT + 4 * rtl->dirty_tx));
+        unused(i);
+        rtl->dirty_tx++;
+        if (rtl->dirty_tx == 5) {
+            rtl->dirty_tx = 0;
+        }
+    }
+}
+
+static int rtl_irq_handler(cpu_registers_t* r) {
+    unused(r);
+
+    list_for_each(node, rtl_list) {
+        rtl8139_iface_t* iface = node->value;
+        rtl_device_interrupt(iface);
+    }
+    return 0;
+}
+
+static void rtl8139_init(uint32_t did) {
+    unused(did);
 }
 
 static void find_rtl8139(uint32_t device, uint16_t vendor_id, uint16_t device_id, void* extra) {
@@ -45,5 +160,6 @@ static void find_rtl8139(uint32_t device, uint16_t vendor_id, uint16_t device_id
 }
 
 void rtl8139_setup(void) {
+    rtl_list = list_create();
     pci_scan(&find_rtl8139, -1, NULL);
 }
