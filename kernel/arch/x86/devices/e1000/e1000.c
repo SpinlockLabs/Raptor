ccb05246eb2163ddedeeaf1657ff761d3514e700
Kenneth Endfinger <kaendfinger@gmail.com>
2017-07-24 11:38:53 -0400

    e1000: detect 0x10d3 to be e1000 compatible

--- kernel/arch/x86/devices/e1000/e1000.c
+++ kernel/arch/x86/devices/e1000/e1000.c
@@ -537,6 +537,7 @@ static void find_e1000(uint32_t device, uint16_t vid, uint16_t did,
         (did == 0x100e ||
             did == 0x1004 ||
             did == 0x100f ||
+            did == 0x10d3 ||
             did == 0x15b8)) {
         e1000_device_init(device);
     }
