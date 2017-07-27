2e56915a7e1ff65ea7f842b79ad12100535e47f0
Kenneth Endfinger <kaendfinger@gmail.com>
2017-07-27 14:25:43 -0400

    liblox: optimize atomic faking

--- kernel/arch/x86/io.c
+++ kernel/arch/x86/io.c
@@ -1,6 +1,6 @@
 #include "io.h"
 
-#if !defined(__COMPCERT__)
+#if defined(__COMPCERT__)
 #define DEFIO(prefix, type, prefix_el) \
     void out ## prefix(uint16_t port, type value) { \
         asm volatile("out" #prefix "%" #prefix_el"0, %w1" :: "a"(value) : "Nd"(port)); \
@@ -10,21 +10,35 @@
         asm volatile("in" #prefix " %w1, %" #prefix_el "0" : "=a"(ret) : "Nd"(port)); \
         return ret; \
     }
-#else
-#define DEFIO(prefix, type, prefix_el) \
-    void out ## prefix(uint16_t port, type value) { \
-        asm volatile("out" #prefix "%" #prefix_el"0, %w1" :: "r"(value) : "r"(port)); \
-    } \
-    type in ## prefix(uint16_t port) { \
-        type ret;\
-        asm volatile("in" #prefix " %w1, %" #prefix_el "0" : "=r"(ret) : "r"(port)); \
-        return ret; \
-    }
-#endif
 
-DEFIO(b, uint8_t, b)
-DEFIO(s, uint16_t, s)
-DEFIO(l, uint32_t,)
+DEFINE_IO(b, uint8_t, b)
+DEFINE_IO(s, uint16_t, s)
+DEFINE_IO(l, uint32_t,)
+#else
+void outb(uint16_t port, uint8_t value) {
+}
+
+uint8_t inb(uint16_t port) {
+    uint8_t value = 0;
+    return value;
+}
+
+void outl(uint16_t port, uint32_t value) {
+}
+
+uint32_t inl(uint16_t port) {
+    uint32_t value = 0;
+    return value;
+}
+
+void outs(uint16_t port, uint16_t value) {
+}
+
+uint16_t ins(uint16_t port) {
+    uint16_t value = 0;
+    return value;
+}
+#endif
 
 void io_wait(void) {
     asm volatile("jmp 1f\n\t"
--- liblox/atomic.h
+++ liblox/atomic.h
@@ -4,20 +4,19 @@
 #pragma once
 
 #if defined(__STDC_NO_ATOMICS__) || __STDC_VERSION__ < 201112L
-#warning We are faking atomics. Do not use this library seriously, it is very unstable.
+
+#ifdef __GNUC__
+#define atomic_fetch_add __sync_add_and_fetch
+#define atomic_fetch_sub __sync_sub_and_fetch
+#define atomic_exchange __sync_lock_test_and_set
+#define atomic_store __sync_lock_test_and_set
+#else
 #define atomic_fetch_add(a, b) (*(a) = *(a) + (b))
 #define atomic_fetch_sub(a, b) (*(a) = *(a) - (b))
-
-#if !defined(__COMPCERT__)
-#define atomic_exchange(a, b) \
-  __extension__({ \
-    unsigned int ___tmp = *(a); *(a) = (b); ___tmp; \
-  })
-#else
 #define atomic_exchange(a, b) (*(a))
+#define atomic_store(a, b) (*(a) = (b))
 #endif
 
-#define atomic_store(a, b) (*(a) = (b))
 #else
 #include <stdatomic.h>
 #endif
