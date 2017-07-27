913494c1e38008af493e2c562dd99481425def83
Kenneth Endfinger <kaendfinger@gmail.com>
2017-07-27 12:45:42 -0400

    liblox: when using CompCert, atomic exchange should just return current value.

--- liblox/atomic.h
+++ liblox/atomic.h
@@ -3,19 +3,18 @@
  */
 #pragma once
 
-#if !defined(__STDC_NO_ATOMICS__) || __STDC_VERSION__ < 201112L
+#if defined(__STDC_NO_ATOMICS__) || __STDC_VERSION__ < 201112L
 #warning We are faking atomics. Do not use this library seriously, it is very unstable.
 #define atomic_fetch_add(a, b) (*(a) = *(a) + (b))
 #define atomic_fetch_sub(a, b) (*(a) = *(a) - (b))
 
-#if !defined(__COMPCERT__)
+#if defined(__COMPCERT__)
 #define atomic_exchange(a, b) \
   __extension__({ \
     unsigned int ___tmp = *(a); *(a) = (b); ___tmp; \
   })
 #else
-#define atomic_exchange(a, b) \
-  (unsigned int ___tmp = *(a), *(a) = (b), ___tmp)
+#define atomic_exchange(a, b) (*(a))
 #endif
 
 #define atomic_store(a, b) (*(a) = (b))
