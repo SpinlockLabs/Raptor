d2089a8da02d5a2d8d3025c90d80d8447d332c07
Kenneth Endfinger <kaendfinger@gmail.com>
2017-07-28 22:25:29 -0400

    liblox: fix strcpy warnings

--- liblox/string/strcpy.c
+++ liblox/string/strcpy.c
@@ -1,8 +1,8 @@
 #include "../string.h"
 
 char* strcpy(char* restrict dest, const char* restrict src) {
-    const unsigned char* s = src;
-    unsigned char* d = dest;
-    while ((*d++ = *s++));
+    const unsigned char* s = (const unsigned char*) src;
+    unsigned char* d = (unsigned char*) dest;
+    while ((*d++ = *s++)) {}
     return dest;
 }
