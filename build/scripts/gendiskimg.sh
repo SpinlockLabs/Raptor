f6b54f8b90b080afe2f57d3bfddc086836ce96e8
Logan Gorence <loganjohngorence@gmail.com>
2018-02-24 19:57:50 -0800

    Detect whether the system has grub2 prefixed GRUB commands.

--- build/scripts/gendiskimg.sh
+++ build/scripts/gendiskimg.sh
@@ -12,6 +12,8 @@ shift
 LOOP=$(losetup -f)
 MKFS="mkfs.ext2"
 SFDISK="sfdisk"
+GRUB_INSTALL="grub-install"
+GRUB2_INSTALL="grub2-install"
 
 if ! which "${MKFS}" > /dev/null 2>&1
 then
@@ -21,6 +23,18 @@ fi
 if ! which "${SFDISK}" > /dev/null 2>&1
 then
   SFDISK="/usr/sbin/sfdisk"
+fi
+
+if ! which "${GRUB_INSTALL}" > /dev/null 2>&1
+then
+  # Fedora prefixes grub commands with grub2
+  if which "${GRUB2_INSTALL}" > /dev/null 2>&1
+  then
+    echo "Using grub2-install"
+    GRUB_INSTALL="${GRUB2_INSTALL}"
+  else
+    GRUB_INSTALL="/usr/bin/grub-install"
+  fi
 fi
 
 rm -rf raptor.img
@@ -34,7 +48,7 @@ losetup -P ${LOOP} raptor.img
 MDIR="$(mktemp -d)"
 mount "${LOOP}p1" "${MDIR}"
 cp -R filesystem/* "${MDIR}/"
-grub-install --target=i386-pc -s --boot-directory="${MDIR}/boot" ${LOOP}
+"${GRUB_INSTALL}" --target=i386-pc -s --boot-directory="${MDIR}/boot" ${LOOP}
 sync
 umount "${MDIR}"
 rm -rf "${MDIR}"
